fb5eb1dc37de6c969283be72e895e9cf96904bc4
wanyulindev <wanyulin052@gmail.com>
2023-10-03 16:02:08 -0700

    Prepare my spreadsheet: READY

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # First commit:
 import requests
 import os
+# import json
+import datetime
 
 GENDER = "female"
 WEIGHT_KG = 57.5
@@ -33,14 +35,35 @@ user_params = {
 headers = {
     "x-app-id": application_id,
     "x-app-key": api_keys,
-    "Content-Type": "application/json"
+    # "Content-Type": "application/json"
 }
 
 response = requests.post(exercise_endpoints,
                          json=user_params, headers=headers)
-print(response.text)
+# print(response.text)
 # print((response.json()))
 # Perfectly done by myself!
 # Small adjust to fit with Dr.Angela's codes.
 #------------------------------------------------------------------------
+data = response.json()
+# print(data)
+for item in data["exercises"]:
+    # print(item)
+    # for key, value in item.items():
+    #     print(key, value)
+    items = {key: value for key, value in item.items()
+             if 0 < list(item.keys()).index(key) < 5}
+    # print(items)
+    # print(type(items))
+
+    # Same as first_5_items, no need to change again:
+    # data = json.dumps(first_5_items)
+    # print(data)
+    # print(type(data))
+
+    # for key, value in first_5_items.items():
+    #     print((key, value).json())
+
+# Prepare my spreadsheet: READY!
+
 
